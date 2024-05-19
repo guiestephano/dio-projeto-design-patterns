0f7c12b085e550354dc4285415091dd640cd3618
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-05-18 22:30:44 -0300

    Atualizando @GeneratedValue

--- design-patterns/src/main/java/com/guiestephano/designpatterns/models/Cliente.java
+++ design-patterns/src/main/java/com/guiestephano/designpatterns/models/Cliente.java
@@ -9,7 +9,7 @@ import jakarta.persistence.ManyToOne;
 @Entity
 public class Cliente {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	@ManyToOne
