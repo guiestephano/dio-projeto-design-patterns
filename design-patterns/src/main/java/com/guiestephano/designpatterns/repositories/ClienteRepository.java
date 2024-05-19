26ea412ba22b4bdb6b339147c250c08faa737b6d
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-05-18 22:29:28 -0300

    Atualizando repositories

--- design-patterns/src/main/java/com/guiestephano/designpatterns/repositories/ClienteRepository.java
+++ design-patterns/src/main/java/com/guiestephano/designpatterns/repositories/ClienteRepository.java
@@ -1,10 +1,10 @@
 package com.guiestephano.designpatterns.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.guiestephano.designpatterns.models.Cliente;
 @Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long>{
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
--- design-patterns/src/main/java/com/guiestephano/designpatterns/repositories/EnderecoRepository.java
+++ design-patterns/src/main/java/com/guiestephano/designpatterns/repositories/EnderecoRepository.java
@@ -1,10 +1,10 @@
 package com.guiestephano.designpatterns.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.guiestephano.designpatterns.models.Endereco;
 @Repository
-public interface EnderecoRepository extends CrudRepository<Endereco, String>{
+public interface EnderecoRepository extends JpaRepository<Endereco, String>{
 
 }
